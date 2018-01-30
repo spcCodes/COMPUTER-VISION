d2d1e01bb81515decc149a4abc2ed77f46d42575
SUMAN PAUL CHOUDHURY <sumanpc2008@gmail.com>
2018-01-30 06:40:58 +0530

    2 files added laodingImages and Loading Videos program

--- loadingVideos.py
+++ loadingVideos.py
@@ -11,14 +11,32 @@ import numpy as np
 
 cap = cv2.VideoCapture(0) #0 refers to only web cam
 
+#use a codec
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+#create a output file for saving the video
+out = cv2.VideoWriter('VideoOutput.avi', fourcc , 20.0 , (640,480))
+
+
 while True:
     ret , frame =cap.read()
-    cv2.imshow('Video Frame',frame )
     
+    #changing the video to gray
+    gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
+    
+    #SAVING THE FRAME
+    out.write(frame)
+    
+    #displaying the video frame
+    cv2.imshow('Video Frame',frame )
+    cv2.imshow('Gray video Frame' , gray)
+    
+    #break the video sequence using any key
     if cv2.waitKey(1)  & 0xFF == ord('q'):
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
 
 #Sometimes the windows doesnot get destroyed so use this
